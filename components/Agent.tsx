8cda65d55feb6da997cdc22ee918f7326c2ee02c
anushkakarmakar-14 <anushkakarmakar04@gmail.com>
2025-07-18 15:30:50 +0530

    updated vapi

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -8,6 +8,8 @@ import { cn } from "@/lib/utils";
 import { vapi } from "@/lib/vapi.sdk";
 import { interviewer } from "@/constants";
 import { createFeedback } from "@/lib/actions/general.action";
+ // for example
+
 
 enum CallStatus {
   INACTIVE = "INACTIVE",
